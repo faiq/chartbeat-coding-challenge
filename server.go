8b334d99e91e122779af2d61363e44be4281e1a3
faiq <faiqrazarizvi@gmail.com>
2015-09-22 17:21:27 -0400

    adds some printing to this to see if it works

--- server.go
+++ server.go
@@ -22,12 +22,17 @@ func MainHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func Poll(updates chan<- request.Page) {
-	ticker := time.NewTicker(updateInterval)
+	ticker := time.NewTicker(pollInterval)
 	go func() {
 		for {
 			select {
 			case <-ticker.C:
-				request.MakeRequest()
+				fmt.Printf("tick tock")
+				err := request.MakeRequest("http://api.chartbeat.com/live/toppages/?apikey=317a25eccba186e0f6b558f45214c0e7&host=gizmodo.com&limit=100", updates)
+				if err != nil {
+					fmt.Printf("Uh")
+					break
+				}
 			}
 		}
 	}()
@@ -36,6 +41,15 @@ func Poll(updates chan<- request.Page) {
 func main() {
 	router := mux.NewRouter()
 	updates := make(chan request.Page) // a channel to pass along updates to
+	Poll(updates)
+	go func() {
+		for {
+			select {
+			case page := <-updates:
+				fmt.Printf("ayy %v", page)
+			}
+		}
+	}()
 	router.HandleFunc("/", MainHandler)
 	n := negroni.Classic()
 	n.UseHandler(router)
